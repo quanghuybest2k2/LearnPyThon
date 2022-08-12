4075d90a177d8689bdb9408b2b0577473515b5e9
Doan Quang Huy <quanghuybest@gmail.com>
2022-08-12 10:17:06 +0700

    hoan vi bang cong tru 2 so

---
+++ HoanVi.py
@@ -0,0 +1,11 @@
+a = int(input("Nhập a >> "))  # 5
+b = int(input("Nhập b >> "))  # 2
+
+print(a)
+print(b)
+print("Sau khi hoán đổi >> ")
+a = a + b  # 7
+b = a - b  # 5
+a = a - b  # 2+
+print(a)
+print(b)
