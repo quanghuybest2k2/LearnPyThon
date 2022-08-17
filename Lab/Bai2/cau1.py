8369fe02aeab5f8c27086ee9aa1099bfb3b7be35
quanghuybest2k2 <quanghuybest@gmail.com>
2022-08-17 14:45:32 +0700

    lab1, cau 1

---
+++ Lab/Bai2/cau1.py
@@ -0,0 +1,11 @@
+#cau a)
+a = int(input("Nhap vao a: "))
+b = int(input("Nhap vao b: "))
+sum = a+b
+print("Tong la",sum)
+#cau b)
+div = a / b
+print("Thuong la: ",div)
+# cau c)
+powfuc = pow(a,b) # or a**b
+print("Mu la: ",powfuc)
